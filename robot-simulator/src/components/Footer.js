c7ddf6d1bf164875e86528105473021932afc8de
OneTwoSz <143705921+OneTwoSz@users.noreply.github.com>
2024-08-19 05:39:16 +1000

    This is final

--- robot-simulator/src/components/Footer.js
+++ robot-simulator/src/components/Footer.js
@@ -4,13 +4,20 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>I'm a quick learner, and if you give me some HTML and CSS, I'll sling them into stunning digital experiences.</p>
-        <nav className="footer-nav">
-          <ul>
-            <li><a href="#simulator">Simulator</a></li>
-            <li><a href="#guide">Guide</a></li>
-          </ul>
-        </nav>
+        <p>Psst!</p>
+        <p>I'm a quick learner, and if you give me some HTML and CSS, I'll sling them into stunning digital experiences.I am ready to make an impact at Bellroy!</p>
+        <a href="https://www.sabarivs.com" target="_blank" rel="noopener noreferrer">
+          <button className="btn">
+            <strong>Portfolio</strong>
+            <div id="container-stars">
+              <div id="stars"></div>
+            </div>
+            <div id="glow">
+              <div className="circle"></div>
+              <div className="circle"></div>
+            </div>
+          </button>
+        </a>
       </div>
     </footer>
   );
